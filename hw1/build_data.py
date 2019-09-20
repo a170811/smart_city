435fba346022dd5e82409a0f7b156449878f5035
yiz <ql3895619@gmail.com>
2019-09-20 21:10:43 +0800

    add dataBuilder of format_type 1

--- hw1/build_data.py
+++ hw1/build_data.py
@@ -1,18 +1,17 @@
-from bisect import bisect_right
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sys import argv
 
-from datetime import datetime, timedelta
 
 def data_format1(tr_x, to_predict):
-    ohe = OneHotEncoder(handle_unknown='ignore').fit(tr_x[:, :3])
-    tr_ohe = ohe.transform(tr_x[:, :3]).toarray()
-    tr_num = tr_x[:, 3:] - [22, 120]
-    predict_ohe = ohe.transform(to_predict[:, :3]).toarray()
-    predict_num = to_predict[:, 3:] - [22, 120]
+    ohe = OneHotEncoder(handle_unknown='ignore').fit(tr_x[:, :-2])
+    tr_ohe = ohe.transform(tr_x[:, :-2]).toarray()
+    tr_num = tr_x[:, -2:] - [22, 120]
+    predict_ohe = ohe.transform(to_predict[:, :-2]).toarray()
+    predict_num = to_predict[:, -2:] - [22, 120]
     return np.concatenate((tr_ohe, tr_num), axis=1), np.concatenate((predict_ohe, predict_num), axis=1)
 
 def data_format2(tr_x, to_predict):
@@ -60,31 +59,34 @@ if '__main__' == __name__:
     df_train['8hrs'] = [hour_classify(time, 8) for time in df_train['time']]
     df_test['8hrs'] = [hour_classify(time, 8) for time in df_test['time']]
 
-    drop = ['Id', 'time', 'content']
-    df_train = df_train.drop(columns=drop)
-    df_test = df_test.drop(columns=drop)
+    cols_train = ['district', 'administration', 'type', 'weekday', '8hrs', 'latitude', 'longitude', 'cost_time']
+    df_train = df_train[cols_train]
+    cols_test = ['district', 'administration', 'type', 'weekday', '8hrs', 'latitude', 'longitude']
+    df_test = df_test[cols_test]
 
-    print(df_train)
     # data
-    # tr_x, tr_y = df_train.iloc[:, :-1].to_numpy(), df_train.iloc[:, -1].to_numpy()
-    # to_predict = df_test.to_numpy()
-    # ohe = OneHotEncoder(handle_unknown='ignore').fit(tr_x[:, :3])
-    #
-    # if 1 == format_type:
-    #     tr_x, to_predict = data_format1(tr_x, to_predict)
-    # else:
+    tr_x, tr_y = df_train.iloc[:, :-1].to_numpy(), df_train.iloc[:, -1].to_numpy()
+    to_predict = df_test.to_numpy()
+
+    if 1 == format_type:
+        tr_x, to_predict = data_format1(tr_x, to_predict)
+    # elif 2 == format_type:
     #     tr_x, to_predict = data_format2(tr_x, to_predict)
-    #
-    # tr_x, te_x, tr_y, te_y = train_test_split(tr_x, tr_y, test_size = 0.1)
-    #
-    # print('tr_x: ', np.shape(tr_x))
-    # print('tr_y: ', np.shape(tr_y))
-    # print('te_x: ', np.shape(te_x))
-    # print('te_y: ', np.shape(te_y))
-    # print('to_predict: ', np.shape(to_predict))
-    #
-    # np.save(f'./tmp/tr_x_{format_type}.npy', tr_x)
-    # np.save(f'./tmp/te_x_{format_type}.npy', te_x)
-    # np.save(f'./tmp/tr_y_{format_type}.npy', tr_y)
-    # np.save(f'./tmp/te_y_{format_type}.npy', te_y)
-    # np.save(f'./tmp/to_predict_{format_type}.npy', to_predict)
+    else:
+        print(f'no format_type: {format_tyupe}')
+        exit()
+
+    print(tr_x, to_predict)
+    tr_x, te_x, tr_y, te_y = train_test_split(tr_x, tr_y, test_size = 0.1)
+
+    print('tr_x: ', np.shape(tr_x))
+    print('tr_y: ', np.shape(tr_y))
+    print('te_x: ', np.shape(te_x))
+    print('te_y: ', np.shape(te_y))
+    print('to_predict: ', np.shape(to_predict))
+
+    np.save(f'./tmp/tr_x_{format_type}.npy', tr_x)
+    np.save(f'./tmp/te_x_{format_type}.npy', te_x)
+    np.save(f'./tmp/tr_y_{format_type}.npy', tr_y)
+    np.save(f'./tmp/te_y_{format_type}.npy', te_y)
+    np.save(f'./tmp/to_predict_{format_type}.npy', to_predict)
