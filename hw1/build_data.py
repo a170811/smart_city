eba1b1270b59a71b2744c3ad26e3d36a1c816e0a
yiz <ql3895619@gmail.com>
2019-09-20 20:06:11 +0800

    optimize hour_classify

--- hw1/build_data.py
+++ hw1/build_data.py
@@ -38,9 +38,8 @@ def data_format2(tr_x, to_predict):
 
     return tr_ohe, predict_ohe
 
-def hour_classify(time):
-    l = list([i for i in range(0, 24, 8)])
-    return bisect_right(l, time.hour)
+def hour_classify(time, num_class):
+    return time.hour // num_class
 
 if '__main__' == __name__:
 
@@ -58,33 +57,34 @@ if '__main__' == __name__:
     df_train['weekday'] = [time.weekday() for time in df_train['time']]
     df_test['weekday'] = [time.weekday() for time in df_test['time']]
 
-    df_train['8hrs'] = [hour_classify(time) for time in df_train['time']]
-    df_test['8hrs'] = [hour_classify(time) for time in df_test['time']]
+    df_train['8hrs'] = [hour_classify(time, 8) for time in df_train['time']]
+    df_test['8hrs'] = [hour_classify(time, 8) for time in df_test['time']]
 
     drop = ['Id', 'time', 'content']
     df_train = df_train.drop(columns=drop)
     df_test = df_test.drop(columns=drop)
 
+    print(df_train)
     # data
-    tr_x, tr_y = df_train.iloc[:, :-1].to_numpy(), df_train.iloc[:, -1].to_numpy()
-    to_predict = df_test.to_numpy()
-    ohe = OneHotEncoder(handle_unknown='ignore').fit(tr_x[:, :3])
-
-    if 1 == format_type:
-        tr_x, to_predict = data_format1(tr_x, to_predict)
-    else:
-        tr_x, to_predict = data_format2(tr_x, to_predict)
-
-    tr_x, te_x, tr_y, te_y = train_test_split(tr_x, tr_y, test_size = 0.1)
-
-    print('tr_x: ', np.shape(tr_x))
-    print('tr_y: ', np.shape(tr_y))
-    print('te_x: ', np.shape(te_x))
-    print('te_y: ', np.shape(te_y))
-    print('to_predict: ', np.shape(to_predict))
-
-    np.save(f'./tmp/tr_x_{format_type}.npy', tr_x)
-    np.save(f'./tmp/te_x_{format_type}.npy', te_x)
-    np.save(f'./tmp/tr_y_{format_type}.npy', tr_y)
-    np.save(f'./tmp/te_y_{format_type}.npy', te_y)
-    np.save(f'./tmp/to_predict_{format_type}.npy', to_predict)
+    # tr_x, tr_y = df_train.iloc[:, :-1].to_numpy(), df_train.iloc[:, -1].to_numpy()
+    # to_predict = df_test.to_numpy()
+    # ohe = OneHotEncoder(handle_unknown='ignore').fit(tr_x[:, :3])
+    #
+    # if 1 == format_type:
+    #     tr_x, to_predict = data_format1(tr_x, to_predict)
+    # else:
+    #     tr_x, to_predict = data_format2(tr_x, to_predict)
+    #
+    # tr_x, te_x, tr_y, te_y = train_test_split(tr_x, tr_y, test_size = 0.1)
+    #
+    # print('tr_x: ', np.shape(tr_x))
+    # print('tr_y: ', np.shape(tr_y))
+    # print('te_x: ', np.shape(te_x))
+    # print('te_y: ', np.shape(te_y))
+    # print('to_predict: ', np.shape(to_predict))
+    #
+    # np.save(f'./tmp/tr_x_{format_type}.npy', tr_x)
+    # np.save(f'./tmp/te_x_{format_type}.npy', te_x)
+    # np.save(f'./tmp/tr_y_{format_type}.npy', tr_y)
+    # np.save(f'./tmp/te_y_{format_type}.npy', te_y)
+    # np.save(f'./tmp/to_predict_{format_type}.npy', to_predict)
